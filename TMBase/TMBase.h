4bccfc291451b9ff12781f09ffcbe99869ae4e4f
TMAker <funnot404@gmail.com>
2018-07-27 18:36:01 +0800

    Initial Commit

---
+++ TMBase/TMBase.h
@@ -0,0 +1,19 @@
+//
+//  TMBase.h
+//  TMBase
+//
+//  Created by Jason Tsai on 2018/7/27.
+//  Copyright © 2018年 TMAker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TMBase.
+FOUNDATION_EXPORT double TMBaseVersionNumber;
+
+//! Project version string for TMBase.
+FOUNDATION_EXPORT const unsigned char TMBaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TMBase/PublicHeader.h>
+
+
---
+++ TMBaseTests/TMBaseTests.swift
@@ -0,0 +1,36 @@
+//
+//  TMBaseTests.swift
+//  TMBaseTests
+//
+//  Created by Jason Tsai on 2018/7/27.
+//  Copyright © 2018年 TMAker. All rights reserved.
+//
+
+import XCTest
+@testable import TMBase
+
+class TMBaseTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
